b595bb8a97e36a5bbb3e02227396f391afea8bc4
juandeaguero <juandeaguero@gmail.com>
2023-06-09 12:25:20 +0200

    docs

--- http-functions.js
+++ http-functions.js
@@ -2,20 +2,37 @@ import wixData from "wix-data";
 import wixStoresBackend from "wix-stores-backend";
 import { ok } from 'wix-http-functions';
 
+// Handles HTTP POST requests
 export async function post_updateInventory(request) {
+
+    // Parse the body of the HTTP request as JSON
     const requestBody = await request.body.json()
+
+    // Query the "Stores/Variants" collection in the Wix database and storing the results in the 'items' variable
     const { items } = await wixData.query("Stores/Variants").find();
+    
+    // Loop through all items from the database
     for (let i = 0; i < items.length; i++) {
+        
+        // Get the new stock value from the request body using the item SKU
         const stock = requestBody[items[i].sku];
+        
+        // If no stock value found for this SKU, skip to the next iteration
         if (!stock) continue;
+        
+        // Calculate the difference between new stock value and the current stock quantity
         const difference = stock - items[i].stock.quantity;
+        
+        // If the difference is positive, increment the inventory with that difference
         if (difference > 0) {
             wixStoresBackend.incrementInventory([{
                 variantId: items[i].variantId,
                 productId: items[i].productId,
                 incrementBy: difference
             }]);
-        } else if (difference < 0) {
+        } 
+        // If the difference is negative, decrement the inventory with the absolute value of that difference
+        else if (difference < 0) {
             wixStoresBackend.decrementInventory([{
                 variantId: items[i].variantId,
                 productId: items[i].productId,
@@ -23,7 +40,13 @@ export async function post_updateInventory(request) {
             }]);
         }
     }
+    
+    // Create a response object
     let response = {};
+    
+    // Add a body to the response indicating that the inventory update has been successful
     response.body = {"wix": "Inventory Updated"};
+    
+    // Return an HTTP 200 OK response, with the previously created response body
     return ok(response);
 }
--- proxy.js
+++ proxy.js
@@ -1,27 +1,46 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-const fs = require("fs");
+// This script uses Node.js and the Express framework to create a simple proxy server.
+// Its purpose is to read an inventory CSV file, format the data into a JSON object,
+// and then send that data to a remote API via a POST request. The server also uses
+// the CORS middleware to allow cross-origin requests, increasing its compatibility
+// with various client-side applications.
 
+const express = require("express");  // Express.js framework for server implementation
+const cors = require("cors");  // Middleware to enable CORS
+const axios = require("axios");  // HTTP client for making requests
+const fs = require("fs");  // File system module for file I/O operations
+
+// Initialize express application
 const app = express();
-app.use(cors());
+app.use(cors());  // Use CORS middleware
 
+// Function to send data to a given API endpoint via a POST request
 const sendData = async (data) => {
     const response = await axios.post(UPDATE_INVENTORY_URL, data);
-    console.log(response.data);
+    console.log(response.data);  // Log response data for debugging purposes
 };
 
+// Define server's listening port. If process.env.PORT is not set, default to port 3000.
 const PORT = process.env.PORT || 3000;
+
+// Start server and log the port it is running on
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+
+    // Read inventory data from a CSV file on the local machine
     fs.readFile("/Users/juan/Desktop/proxy-server/inventario.csv", "utf8", (err, data) => {
-        let splitData = data.split(";");
+        let splitData = data.split(";");  // Split CSV data by semi-colon
         jsonData = {};
+
+        // Iterate over splitData array, step size of 8, starting from the 10th element
         for (let i = 9; i < splitData.length; i += 8) {
+
+            // If the inventory count is greater than 0, add the product to jsonData
             if (parseInt(splitData[i]) > 0) {
-                jsonData[splitData[i]] = splitData[i + 1];
+                jsonData[splitData[i]] = splitData[i + 1];  // Key-value pair of productID-quantity
             }
         }
+
+        // Send jsonData to the UPDATE_INVENTORY_URL via a POST request
         sendData(jsonData);
     });
 });
